cc5fe7c9a0dd1728d5ac9d8f4232c52952805eff
aa-ag <aaron.aguerrevere@gmail.com>
2022-10-09 18:45:01 -0500

    Refactor into authenticate\(\)

--- script.py
+++ script.py
@@ -9,23 +9,27 @@ bbbUSERNAME = os.environ["bbbUSERNAME"]
 bbbPASSWORD = os.environ["bbbPASSWORD"]
 bbbTOKEN = os.environ["bbbTOKEN"]
 
-url = 'https://api.bbb.org/token'
 
-headers = {
-        'content-Type': 'application/x-www-form-urlencoded'
-    }
+def authenticate():
+    url = 'https://api.bbb.org/token'
 
-request_body = "grant_type=password&username={0}&password={1}".format(
-    bbbUSERNAME, bbbPASSWORD
-)
+    headers = {
+            'Authorization': f'Bearer {bbbTOKEN}',
+            'content-Type': 'application/x-www-form-urlencoded'
+        }
 
-request_body = json.dumps(request_body)
-print(request_body)
-
-r = requests.get(
-        url,
-        headers=headers,
-        data=request_body,
+    request_body = "grant_type=password&username={0}&password={1}".format(
+        bbbUSERNAME, bbbPASSWORD
     )
 
-print(r)
+    r = requests.get(
+            url,
+            headers=headers,
+            data=request_body,
+        )
+
+    print(r)
+
+
+if __name__ == "__main__":
+    authenticate()
