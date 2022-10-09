c853e81e405d487ffc238a6fda2f1940c7cf016b
aa-ag <aaron.aguerrevere@gmail.com>
2022-10-09 18:59:19 -0500

    Refactor authenticate + make password edits, now authenticating

--- script.py
+++ script.py
@@ -13,6 +13,27 @@ bbbTOKEN = os.environ["bbbTOKEN"]
 def authenticate():
     url = 'https://api.bbb.org/token'
 
+    header = {
+            'content-type': 'application/x-www-form-url-encoded'
+        }
+
+    request_body = "grant_type=password&username={0}&password={1}".format(
+        bbbUSERNAME, bbbPASSWORD
+    )
+
+    r = requests.post(
+            url,
+            headers=header,
+            data=request_body,
+        )
+
+    print(r.status_code)
+    print(r.headers)
+    print(r.content)
+
+
+def search_org():
+    url = "https://api.bbb.org/api/orgs/search"
     headers = {
             'Authorization': f'Bearer {bbbTOKEN}',
             'content-Type': 'application/x-www-form-urlencoded'
@@ -22,14 +43,12 @@ def authenticate():
         bbbUSERNAME, bbbPASSWORD
     )
 
-    r = requests.get(
-            url,
-            headers=headers,
-            data=request_body,
-        )
+    r = requests.get(url,headers=headers,data=request_body)
 
-    print(r)
+    print(r.headers)
+    print(r.content)
 
 
 if __name__ == "__main__":
     authenticate()
+    # search_org()
