f9b00fda9377b94baf2e76b62a81590a5a4544ec
aa-ag <aaron.aguerrevere@gmail.com>
2022-10-08 21:54:04 -0500

    Add request_body

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import requests
 from dotenv import load_dotenv
 import os
+import json
 
 load_dotenv()
 
@@ -8,15 +9,19 @@ bbbUSERNAME = os.environ["bbbUSERNAME"]
 bbbPASSWORD = os.environ["bbbPASSWORD"]
 
 url = 'https://api.bbb.org/token'
+
 headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
+        'content-Type': 'application/x-www-form-urlencoded'
     }
+
+request_body = "password&username={0}&password={1}".format(
+    bbbUSERNAME, bbbPASSWORD
+)
+
 r = requests.post(
         url,
         headers=headers,
-        data=f'grant_type=password&username={0}&password={1}'.format(
-            bbbUSERNAME,bbbPASSWORD
-        )
+        data=request_body,
     )
 
 print(r)
