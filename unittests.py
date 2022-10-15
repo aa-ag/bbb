99a6aa9fd71fb70478a4517336c622bb42fc96c3
aa-ag <aaron.aguerrevere@gmail.com>
2022-10-15 12:12:07 -0500

    Add test_password_exists & change methods' names to include work test to be able to run

--- unittests.py
+++ unittests.py
@@ -1,15 +1,20 @@
 import unittest
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 class TestScript(unittest.TestCase):
-    def is_there_a_token(self):
-        from dotenv import load_dotenv
-        import os
-        
-        load_dotenv()
-
+    def test_username_exists(self):
         bbbUSERNAME = os.environ["bbbUSERNAME"]
 
         self.assertIsNotNone(bbbUSERNAME)
+    
+    def test_password_exists(self):
+        bbbPASSWORD = os.environ["bbbPASSWORD"]
+
+        self.assertIsNotNone(bbbPASSWORD)
+
 
 
 if __name__ == "__main__":
