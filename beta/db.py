3f46349a3a7b60e55fe07d9942e02f3346ec9505
aa-ag <aaron.aguerrevere@gmail.com>
2022-10-22 18:45:38 -0500

    Turn connect to db into a  function

--- beta/db.py
+++ beta/db.py
@@ -1,33 +1,38 @@
 import psycopg2
 from config import user,password,host,port,database
 
-print(user,password,host,port,database)
+# print(user,password,host,port,database)
 
 connection = None
 
-try:
-    connection = psycopg2.connect(
-        user=user,
-        password=password,
-        host=host,
-        port=port,
-        database=database,
-    )
+def connect_to_db():
+    try:
+        connection = psycopg2.connect(
+            user=user,
+            password=password,
+            host=host,
+            port=port,
+            database=database,
+        )
 
-    cursor = connection.cursor()
+        cursor = connection.cursor()
 
-    print("PostgreSQL server information")
+        print("PostgreSQL server information")
 
-    cursor.execute("SELECT version();")
+        cursor.execute("SELECT version();")
 
-    record = cursor.fetchone()
+        record = cursor.fetchone()
 
-    print("Connected to ", record)
+        print("Connected to ", record)
 
-except (Exception, psycopg2.Error) as error:
-    print(error)
+    except (Exception, psycopg2.Error) as error:
+        print(error)
 
-if connection:
-    cursor.close()
-    connection.close()
-    print("Connection closed.")
+    if connection:
+        cursor.close()
+        connection.close()
+        print("Connection closed.")
+
+
+if __name__ == "__main__":
+    connect_to_db()
