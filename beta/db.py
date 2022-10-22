c49da8a343f030876fdb1b7875476e5ad999be56
aa-ag <aaron.aguerrevere@gmail.com>
2022-10-22 18:44:46 -0500

    Establish Postgresql connection and test that it works

--- beta/db.py
+++ beta/db.py
@@ -3,3 +3,31 @@ from config import user,password,host,port,database
 
 print(user,password,host,port,database)
 
+connection = None
+
+try:
+    connection = psycopg2.connect(
+        user=user,
+        password=password,
+        host=host,
+        port=port,
+        database=database,
+    )
+
+    cursor = connection.cursor()
+
+    print("PostgreSQL server information")
+
+    cursor.execute("SELECT version();")
+
+    record = cursor.fetchone()
+
+    print("Connected to ", record)
+
+except (Exception, psycopg2.Error) as error:
+    print(error)
+
+if connection:
+    cursor.close()
+    connection.close()
+    print("Connection closed.")
