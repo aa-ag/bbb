161427a9c155fc583935d7e3da100ce3072dc509
aa-ag <aaron.aguerrevere@gmail.com>
2022-10-23 18:29:44 -0500

    Refactor db connection setup

--- beta/db.py
+++ beta/db.py
@@ -6,33 +6,39 @@ from config import user,password,host,port,database
 connection = None
 
 def connect_to_db():
+    connection = psycopg2.connect(
+        user=user,
+        password=password,
+        host=host,
+        port=port,
+        database=database,
+    )
+
+    cursor = connection.cursor()
+    return cursor
+
+def test_connect(cur):
     try:
-        connection = psycopg2.connect(
-            user=user,
-            password=password,
-            host=host,
-            port=port,
-            database=database,
-        )
-
-        cursor = connection.cursor()
-
         print("PostgreSQL server information")
 
-        cursor.execute("SELECT version();")
+        cur.execute("SELECT version();")
 
-        record = cursor.fetchone()
+        record = cur.fetchone()
 
         print("Connected to ", record)
+        return record
 
     except (Exception, psycopg2.Error) as error:
         print(error)
 
-    if connection:
-        cursor.close()
-        connection.close()
-        print("Connection closed.")
+
+# def close_connection():
+#     if connection:
+#         cursor.close()
+#         connection.close()
+#         print("Connection closed.")
 
 
 if __name__ == "__main__":
-    connect_to_db()
+    cur = connect_to_db()
+    test_connect(cur)
